07a74687573c4ee5710e9147ba809bdb2bba4acf
Maria Stelmakh <maniastelmah@gmail.com>
2024-03-19 22:28:17 +0400

    fixed send from test

--- src/test/java/configs/WebDriverConfig.java
+++ src/test/java/configs/WebDriverConfig.java
@@ -9,23 +9,23 @@ import org.aeonbits.owner.Config;
 })
 public interface WebDriverConfig extends Config {
     @Key("baseUrl")
-   // @DefaultValue("https://simtechdev.ru")
+    @DefaultValue("https://simtechdev.ru")
     String getBaseUrl();
 
     @Key("browser.name")
-  //  @DefaultValue("chrome")
+    @DefaultValue("chrome")
     String getBrowserName();
 
     @Key("version")
-//    @DefaultValue("122.0")
+    @DefaultValue("122.0")
     String getBrowserVersion();
 
     @Key("browserSize")
-//    @DefaultValue("1920x1080")
+    @DefaultValue("1920x1080")
     String getBrowserSize();
 
     @Key("remoteUrl")
-//    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
     String getRemoteUrl();
 
     @DefaultValue("false")
